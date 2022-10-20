0ba07b640505dd188742a990596f461361934f81
cristian m <camunoz2@gmail.com>
2022-10-20 11:49:41 -0300

    added menu transitions

--- components/UI/Menu.tsx
+++ components/UI/Menu.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+
 const Menu = () => {
+  const line = 'w-10 h-1 bg-gray-700 transition-all'
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="absolute left-10 top-10 z-20 flex gap-2">
-      <div className="w-12 h-2 bg-gray-80 0" />
-      <div className="w-12 h-2 bg-gray-80 0" />
-      <div className="w-12 h-2 bg-gray-800" />
+    <div
+      onClick={() => setOpen(!open)}
+      className="absolute right-10 top-10 z-20"
+    >
+      <div className="flex flex-col gap-1">
+        <div className={`${line} ${open ? 'rotate-45 translate-y-2' : ''}`} />
+        <div className={`${line} ${open ? 'opacity-0' : ''}`} />
+        <div className={`${line} ${open ? '-rotate-45 -translate-y-2' : ''}`} />
+      </div>
     </div>
   )
 }
